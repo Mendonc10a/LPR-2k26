af8336890def92706f15c3d67e85d9dca5328650
Mendonc10a <joaogcmendonca@gmail.com>
2026-04-07 13:40:48 -0300

    segundocommit

---
+++ Aula05/exercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Digite a quantidade de números: ");
+int totalParaDigitar = int.Parse(Console.ReadLine());
+
+double somaPares = 0;
+int contadorPares = 0;
+int i = 1;
+
+while (i <= totalParaDigitar)
+{
+    Console.Write($"Digite o {i}º número: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num % 2 == 0)
+    {
+        somaPares += num;
+        contadorPares++;
+    }
+    i++; 
+}
+
+if (contadorPares > 0)
+{
+    Console.WriteLine($"\nMédia dos pares: {somaPares / contadorPares}");
+}
+else
+{
+    Console.WriteLine("\nNenhum par digitado.");
+}
