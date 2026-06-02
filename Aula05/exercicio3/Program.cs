7f6c79d8ef04a1cdbe164b60db0b0abc4d3f5c6c
Mendonc10a <joaogcmendonca@gmail.com>
2026-06-02 09:15:42 -0300

    segundocommit

---
+++ Aula05/exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int soma = 0;
+
+            for (int i = 50; i <= 500; i++)
+            {
+                if (i % 3 == 0 && i % 2 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
